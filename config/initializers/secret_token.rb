836b023b0d266f817226b4a8dda8a123d81af5f4
Reza Jelveh <toyb0mb@gmx.net>
2013-01-28 02:24:06 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codejammin::Application.config.secret_token = '6fe8fcb0bbedf726adf4db06ccbfd6b20a4e4d581919aa417f272cfb1bb7429d7ab120b7b2d8de5b01093c09703433176660612649093f9d417a9679898a4759'

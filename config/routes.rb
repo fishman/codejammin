7e1d80205294f45dc92675b8a21dda6c99b6faf4
Reza Jelveh <toyb0mb@gmx.net>
2013-01-29 18:54:33 -0600

    you know, some updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Codejammin::Application.routes.draw do
+  resources :events
+
+
   authenticated :user do
     root :to => 'home#index'
   end
   root :to => "home#index"
-  devise_for :users
+  devise_for :users, :skip => [:sessions]
   resources :users
 end
